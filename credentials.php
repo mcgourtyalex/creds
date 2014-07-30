75d2db9dd8b66d64e6b0d31d39c16553a9f62033
mcgourtyalex <mcgourty.alex@berkeley.edu>
2014-07-30 16:07:53 -0700

    creds and explanation page

--- credentials.php
+++ credentials.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * Plugin Name: Google Analytics Page List
+ * Plugin Name: Credentials
  * Plugin URI: 
- * Description: Lists ALL your website's pages and the number of views received
+ * Description: Lists EHP's credentials
  * Version: 1.0
  * Author: Alex McGourty
  * Author URI: 
  * License: GLP2
  */
-?>
-<?php
+
  /*  Copyright 2014  Alex McGourty  (email : mcgourty.alex@gmail.com)
 
     This program is free software; you can redistribute it and/or modify
@@ -25,38 +24,30 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-?>
 
-<?php
-
-define( 'PATH', plugin_dir_path( __FILE__ ) );
-
-// Settings for adding Client_ID
-require_once( PATH . '\options.php' );
+define('CREDS_PATH', plugin_dir_path( __FILE__ ) );
 
 // Widget functions page
-require_once( PATH . '\custom_widgets.php' );
-require_once( PATH . '\column_width.php' );
+require_once( CREDS_PATH . '/creds_content.php' );
 
 // Main dashboard class
-class dashboard_widget {
+class creds {
     
     // Constructor adds the action
     function __construct() {
-        add_action( 'wp_dashboard_setup', array( $this, 'add_pages_widget' ) );
-        add_action('admin_head','cw_change_dashboard_column_width');
+        add_action( 'wp_dashboard_setup', array( $this, 'add_credentials_widget' ) );
     }
 
     // Callback function to add widget to dashboard
-    function add_pages_widget() {
+    function add_credentials_widget() {
         wp_add_dashboard_widget(
-                'google-analytics-page-list',
-                'Google Analytics Pages',
-                'pages_widget_content');
+                'credentials',
+                'EHP Google Analytics Credentials',
+                'credentials_widget_content');
     }
 
 }
  
 // Create the class
-$dash = new dashboard_widget();
+$dash = new creds();
 ?>
---
+++ creds_content.php
@@ -0,0 +1,10 @@
+<?php
+
+// Populate the widget:
+function credentials_widget_content() {
+
+    echo '<table><tr><td><strong>User:</strong></td><td>ehp.auto@gmail.com</td></tr>
+                 <tr><td><strong>Pass:</strong></td><td>autodeskengineering</td></tr></table>';
+    echo '<a target="_blank" href="../wp-content/plugins/credentials/demo/demo.html">?</a>';
+}
+?>
